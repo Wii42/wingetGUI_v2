9b1ead13e80fa6465ccf0377875afc215e78fe3d
Wi42 <101128608+Wii42@users.noreply.github.com>
2023-06-17 02:22:52 +0200

    made history entries prettier

---
+++ lib/history_entry.dart
@@ -0,0 +1,44 @@
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:winget_gui/content/content_snapshot.dart';
+import 'package:winget_gui/widget_assets/run_button_tooltip.dart';
+
+import 'content/content_pane.dart';
+
+class HistoryEntry extends StatelessWidget {
+  final ContentSnapshot snapshot;
+  final ContentPane content;
+
+  const HistoryEntry(
+      {super.key, required this.snapshot, required this.content});
+
+  @override
+  Widget build(BuildContext context) {
+    FluentThemeData theme = FluentTheme.of(context);
+
+    return RunButtonTooltip(
+      command: snapshot.command,
+      useMousePosition: true,
+      button: Button(
+        onPressed: () {
+          content.showResultOfCommand(snapshot.command);
+        },
+        child: Padding(
+          padding: EdgeInsetsDirectional.symmetric(vertical: 5),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                snapshot.title,
+                style: theme.typography.bodyStrong,
+              ),
+              Text(
+                'winget ${snapshot.command.join(' ')}',
+                style: TextStyle(color: theme.disabledColor),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/history_tab.dart
+++ lib/history_tab.dart
@@ -1,7 +1,7 @@
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:winget_gui/content/content_holder.dart';
 import 'package:winget_gui/helpers/extensions/widget_list_extension.dart';
-import 'package:winget_gui/widget_assets/run_button_tooltip.dart';
+import 'package:winget_gui/history_entry.dart';
 import 'package:winget_gui/widget_assets/scroll_list_widget.dart';
 
 import 'content/content_snapshot.dart';
@@ -20,19 +20,7 @@ class HistoryTab extends PaneItem {
       body: ScrollListWidget(
         listElements: [
           for (ContentSnapshot snapshot in contentHolder.stack.asList.reversed)
-            RunButtonTooltip(
-              command: snapshot.command,
-              useMousePosition: true,
-              button: Button(
-                onPressed: () {
-                  contentHolder.content.showResultOfCommand(snapshot.command);
-                },
-                child: Text(
-                  snapshot.title,
-                  textAlign: TextAlign.start,
-                ),
-              ),
-            )
+            HistoryEntry(snapshot: snapshot, content: contentHolder.content)
         ].withSpaceBetween(height: 10),
       ),
     );
