ea3ce1ab22686edad1c44848ce2422d5db41c6e9
Wi42 <101128608+Wii42@users.noreply.github.com>
2023-06-26 14:40:18 +0200

    internationalized "show in store"

--- lib/widget_assets/store_button.dart
+++ lib/widget_assets/store_button.dart
@@ -1,17 +1,18 @@
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:open_store/open_store.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class StoreButton extends StatelessWidget {
   final String storeId;
-  final Text text;
+  final String? text;
 
-  const StoreButton(
-      {super.key, required this.storeId, this.text = const Text("Show in Store")});
+  const StoreButton({super.key, required this.storeId, this.text});
 
   @override
   Widget build(BuildContext context) {
+    AppLocalizations locale = AppLocalizations.of(context)!;
     return Button(
-        child: text,
+        child: Text(text ?? locale.showInStore),
         onPressed: () {
           OpenStore.instance.open(windowsProductId: storeId);
         });
