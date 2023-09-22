81d43c3aa82fff88871ddf4209f3cabd5c57b324
Wi42 <101128608+Wii42@users.noreply.github.com>
2023-09-23 01:32:33 +0200

    (un)install successful is now shown with infoBar

--- lib/output_handling/plain_text/plain_text_parser.dart
+++ lib/output_handling/plain_text/plain_text_parser.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:winget_gui/helpers/extensions/string_list_extension.dart';
+import 'package:winget_gui/helpers/extensions/widget_list_extension.dart';
 import 'package:winget_gui/output_handling/output_builder.dart';
 
 import '../../widget_assets/link_text.dart';
@@ -17,16 +18,28 @@ class PlainTextParser extends OutputParser {
     if (lines.isEmpty) {
       return null;
     }
-    return QuickOutputBuilder(
-      (context) => Padding(
+    return QuickOutputBuilder((context) {
+      return Padding(
         padding: const EdgeInsetsDirectional.symmetric(horizontal: 5),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            for (String line in lines) LinkText(line: line),
-          ],
+            for ((int, String) line in lines.indexed)
+              isSuccessMessage(line, wingetLocale)
+                  ? InfoBar(
+                      title: Text(line.$2),
+                      severity: InfoBarSeverity.success,
+                    )
+                  : LinkText(line: line.$2),
+          ].withSpaceBetween(height: 10),
         ),
-      ),
-    );
+      );
+    });
+  }
+
+  bool isSuccessMessage((int, String) line, AppLocalizations locale) {
+    return line.$1 == lines.length - 1 &&
+        (line.$2 == locale.installSuccessful ||
+            line.$2 == locale.uninstallSuccessful);
   }
 }
