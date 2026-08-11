97936b00e74fdbf2dcb6583724fb565b0f307399
Wi42 <luki.kuenzi@gmail.com>
2026-08-12 00:13:01 +0200

    update pubspec.lock dependencies

--- dart_scripts/ckeck_remote_screenshots.dart
+++ dart_scripts/ckeck_remote_screenshots.dart
@@ -2,10 +2,10 @@ import 'package:winget_core/winget_core.dart';
 import 'package:winget_gui/server_interface/unigetui_server_interface.dart';
 import 'package:winget_gui/server_interface/server_interface.dart';
 
-void main()async {
+void main() async {
   ServerInterface serverInterface = UniGetUIServerInterface();
-  Map<String, PackageScreenshots> screenshots = await serverInterface.fetchPackageScreenshotsFromServer();
+  Map<String, PackageScreenshots> screenshots =
+      await serverInterface.fetchPackageScreenshotsFromServer();
   print("Fetched ${screenshots.length} screenshots from server:");
   screenshots.entries.forEach(print);
-
 }
